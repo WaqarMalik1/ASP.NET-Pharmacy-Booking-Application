342e7d8376fa033a957f5c5f65e5c66dc77f5c52
WaqarMalik1 <waqar007@hotmail.co.uk>
2016-03-30 00:34:05 +0100

    Added files via upload

---
+++ Login.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="FinalYearProject.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style6 {
+            font-weight: bold;
+            font-size: large;
+        }
+        .auto-style7 {
+            text-align: center;
+        }
+        </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="auto-style7">
+    
+        <asp:Image ID="Image1" runat="server" Height="275px" ImageAlign="Middle" ImageUrl="~/Images/thelogo.jpg" style="margin-left: 36px; margin-top: 0px" Width="703px" />
+        <br />
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="caption" runat="server" Font-Names="Georgia" style="font-weight: 700; font-style: italic; font-size: xx-large;" Text="Please log in to the delivery application" ForeColor="Maroon"></asp:Label>
+        <br />
+        <br />
+        <br />
+        <br />
+
+        <asp:Label ID="username" runat="server" CssClass="auto-style6" Text="User Name:" Width="100px"></asp:Label>
+        <asp:TextBox ID="TextBoxUserName" runat="server" Height="25px" Width="200px" AutoCompleteType="Disabled"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="TextBoxUserName" ErrorMessage="PLEASE ENTER USERNAME!" ForeColor="Red" Height="25px" style="font-weight: 700"></asp:RequiredFieldValidator>
+
+        <br />
+        <br />
+
+        <asp:Label ID="password" runat="server" CssClass="auto-style6" Text="Password:" Width="100px"></asp:Label>
+        <asp:TextBox ID="TextBoxPassword" runat="server" Height="25px" Width="200px" TextMode="Password" AutoCompleteType="Disabled"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="TextBoxPassword" ErrorMessage="PLEASE ENTER PASSOWRD!" ForeColor="Red" Height="25px" style="font-weight: 700"></asp:RequiredFieldValidator>
+
+        <br />
+        <br />
+        <asp:Button ID="Button_Login" runat="server" Height="40px" OnClick="Button_Login_Click" Text="SIGN IN" Width="400px" />
+        <br />
+        <br />
+        <asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl="~/Registration.aspx">CLICK HERE TO REGISTER FOR NEW STAFF MEMBERS TO USE THE DELIVERY PORTAL</asp:HyperLink>
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient; //This provides all the classes of SQL
+using System.Configuration; //contains the types that provide the programming model for handling configuration data.
+using System.Data; //enables you to manage data from multiple data sources.
+
+namespace FinalYearProject //my namespace project name
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button_Login_Click(object sender, EventArgs e)
+        {
+            //connects registrtion form with database
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString);
+
+            //Opens a database connection with the property settings specified by the ConnectionString
+            conn.Open();
+
+            //This is a query selects count fron table and checks wheather new username is in the database or not
+            string checkuser = "select count(*) from [Table] where UserName=@UserName";
+
+            //passing string checkuser here and conn declaring SQL command
+            SqlCommand com = new SqlCommand(checkuser, conn);
+            
+            //com.Parameters.Add("@UserName", SqlDbType.NChar, 20).Value = TextBoxUserName.Text + "'";
+            //Adds username to database
+            com.Parameters.Add("@UserName", SqlDbType.NChar, 20).Value = TextBoxUserName.Text;
+
+            //temp gets converted to string
+            int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
+           // int temp = Convert.ToInt32(com.ExecuteScalar());
+            
+            //closes query
+            conn.Close();
+
+            //if statement declared
+            if (temp == 1)
+            {
+                //opens query connection
+                conn.Open();
+
+                //This is a query selects password from table and checks wheather  username is in the database or not
+                string checkPasswordQuery = " select password from [Table] where UserName=@UserName";
+
+                //passwordquery string passed here
+                SqlCommand passComm = new SqlCommand(checkPasswordQuery, conn);
+
+                //gets username parameter adds this in textbox/table
+                passComm.Parameters.Add("@UserName", SqlDbType.NVarChar, 20).Value = TextBoxUserName.Text;
+
+                //replaces spaces
+                string password = passComm.ExecuteScalar().ToString().Replace(" ", "");
+
+                //if statement declared
+                if (password == TextBoxPassword.Text)
+                {
+
+                    //declaring new session
+                    Session["New"] = TextBoxUserName.Text;
+
+                    //gives message
+                    Response.Write("PASSWORD IS CORRECT");
+
+                    //redirects to page
+                    Response.Redirect("afterregistrationpage1.aspx");
+                }
+                //else statment
+                else
+                {
+                //says this message
+                 Response.Write("PASSWORD IS NOT CORRECT");
+                }
+                }
+                     //ese statement
+                     else
+                    {
+                //syays this message
+                Response.Write("USERNAME IS NOT CORRECT");
+                }
+}
+    }
+} 
